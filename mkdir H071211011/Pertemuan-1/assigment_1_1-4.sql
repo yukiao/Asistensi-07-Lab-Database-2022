9bbf2097e4a3a534624a22f2f5ee8d038449d15b
bulllqis <bulqisramadani.4@gmail.com>
2022-09-22 20:46:58 +0800

    menambah tugas 1 praktikum data base

---
+++ mkdir H071211011/Pertemuan-1/assigment_1_1-4.sql
@@ -0,0 +1,23 @@
+-- CREATE DATABASE kantor
+
+-- CREATE TABLE offices(
+-- officeCode VARCHAR (50) NOT NULL,
+-- city VARCHAR (50) NOT NULL,
+-- phone VARCHAR (50) NOT NULL,
+-- adressline1 VARCHAR (50) NOT NULL,
+-- adressline2 VARCHAR (50),
+-- state VARCHAR (50),
+-- country VARCHAR (50) NOT NULL,
+-- PRIMARY KEY (officeCode) 
+-- )
+
+-- DESCRIBE offices
+
+-- ALTER TABLE offices
+-- MODIFY phone INT(20) NOT NULL
+
+-- ALTER TABLE offices
+-- DROP adressline2
+DESCRIBE offices
+
+
