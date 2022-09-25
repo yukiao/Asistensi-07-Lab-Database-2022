50e1ed143ce5161b5150895d69a086ec3433537b
WdAnandaLesmono <nandalesmono@gmail.com>
2022-09-25 15:02:44 +0800

    Perbaikan Tugas 1

--- H071211074/Pertemuan-1/assignment_1_1.sql
+++ H071211074/Pertemuan-1/assignment_1_1.sql
@@ -1,20 +1,24 @@
 USE praktikum1;
--- 
--- CREATE TABLE offices(
--- 	officeCode VARCHAR (10) NOT NULL,
--- 	city VARCHAR (50) NOT NULL,
--- 	phone VARCHAR (50) NOT NULL,
--- 	addressline1 VARCHAR (50) NOT NULL,
--- 	addressline2 VARCHAR (50),
--- 	state VARCHAR(50),
--- 	country VARCHAR(50) NOT NULL,
--- 	PRIMARY KEY (officeCode)
--- );
-praktikum1praktikum1praktikum1
+
+#No.1 
+CREATE TABLE offices(
+	officeCode VARCHAR (10) NOT NULL,
+	city VARCHAR (50) NOT NULL,
+	phone VARCHAR (50) NOT NULL,
+	addressline1 VARCHAR (50) NOT NULL,
+	addressline2 VARCHAR (50),
+	state VARCHAR(50),
+	country VARCHAR(50) NOT NULL,
+	PRIMARY KEY (officeCode)
+);
+
+#No.2
 DESCRIBE offices;
--- 
--- ALTER TABLE offices
--- MODIFY phone INT (20);
--- 
--- ALTER TABLE offices
--- DROP addressline2
+
+#No.3
+ALTER TABLE offices
+MODIFY phone INT (20);
+
+#NO.4
+ALTER TABLE offices
+DROP addressline2
